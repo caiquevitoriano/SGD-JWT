89b68fda8700a9594edf5d2014f412bb9e6ee0a5
caiquevitoriano <vitorianocaique@gmail.com>
2019-03-21 13:34:15 -0300

    criando projeto com spring

---
+++ src/main/java/exemplo/SGDJWT/SgdJwtApplication.java
@@ -0,0 +1,13 @@
+package exemplo.SGDJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgdJwtApplication.class, args);
+	}
+
+}
