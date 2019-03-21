70cecc148f9336c56c7653a84c5721a80d83ce40
caiquevitoriano <vitorianocaique@gmail.com>
2019-03-21 13:43:05 -0300

    criando rota home

--- src/main/java/exemplo/SGDJWT/SgdJwtApplication.java
+++ src/main/java/exemplo/SGDJWT/SgdJwtApplication.java
@@ -1,13 +1,24 @@
 package exemplo.SGDJWT;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class SgdJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgdJwtApplication.class, args);
+	}
+
+	// rota "/home"  verifica o funcionamento correto da aplicação
+	@RequestMapping("/home")
+	public String hello() {
+		return "Hello buddy!";
 	}
 
 }
