348e8d2baaf589bb6905876299dc8178c41cbdd2
caiquevitoriano <vitorianocaique@gmail.com>
2019-03-21 19:32:20 -0300

    filtos de rotas

--- src/main/java/exemplo/Security/JWTAuthenticationFilter.java
+++ src/main/java/exemplo/Security/JWTAuthenticationFilter.java
@@ -1,11 +1,24 @@
 package exemplo.Security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-public class JWTAuthenticationFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+public class JWTAuthenticationFilter extends GenericFilterBean {
 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        Authentication authentication = TokenAuthenticationService
+                .getAuthentication((HttpServletRequest) request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
     }
+
 }
